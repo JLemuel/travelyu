34a9c4d7f8096df2a8a5d77035a841f005a25428
JLemuel <79501195+JLemuel@users.noreply.github.com>
2024-05-27 16:27:37 +0800

    Update BlogPostsChart.php

--- app/Filament/Widgets/BlogPostsChart.php
+++ app/Filament/Widgets/BlogPostsChart.php
@@ -6,11 +6,11 @@ use App\Models\Booking;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\Auth;
 
 class BlogPostsChart extends ChartWidget
 {
-
-    protected static ?string $heading = 'No. of People Entering La Union';
+    protected static ?string $heading = null;
 
     protected int | string | array $columnSpan = 'full';
 
@@ -22,6 +22,12 @@ class BlogPostsChart extends ChartWidget
 
     public ?string $filter = '3months';
 
+    public function getHeading(): string
+    {
+        $user = auth()->user();
+        return $user->email === 'admin@info.com' ? 'No. of People Entering La Union' : 'No. of Tourists Who Book';
+    }
+
     protected function getFilters(): ?array
     {
         return [
@@ -30,40 +36,68 @@ class BlogPostsChart extends ChartWidget
             '3months' => 'Last 3 Months',
         ];
     }
+
     protected function getData(): array
     {
         $filter = $this->filter;
+        $user = auth()->user();
 
-        match ($filter) {
-            'week' => $data = Trend::model(Booking::class)
-                ->between(
-                    start: now()->subWeek(),
-                    end: now(),
-                )
-                ->perDay()
-                ->sum('adults_count'),
-            'month' => $data = Trend::model(Booking::class)
-                ->between(
-                    start: now()->subMonth(),
-                    end: now(),
-                )
-                ->perDay()
-                ->sum('adults_count'),
-            '3months' => $data = Trend::model(Booking::class)
-                ->between(
-                    start: now()->subMonths(3),
-                    end: now(),
-                )
-                ->perMonth()
-                ->sum('adults_count'),
-        };
-
-        // dd($data);
+        if ($user->email === 'admin@info.com') {
+            $chartType = 'bar';
+            match ($filter) {
+                'week' => $data = Trend::model(Booking::class)
+                    ->between(
+                        start: now()->subWeek(),
+                        end: now(),
+                    )
+                    ->perDay()
+                    ->sum('adults_count'),
+                'month' => $data = Trend::model(Booking::class)
+                    ->between(
+                        start: now()->subMonth(),
+                        end: now(),
+                    )
+                    ->perDay()
+                    ->sum('adults_count'),
+                '3months' => $data = Trend::model(Booking::class)
+                    ->between(
+                        start: now()->subMonths(3),
+                        end: now(),
+                    )
+                    ->perMonth()
+                    ->sum('adults_count'),
+            };
+        } else {
+            $chartType = 'line';
+            match ($filter) {
+                'week' => $data = Trend::model(Booking::class)
+                    ->between(
+                        start: now()->subWeek(),
+                        end: now(),
+                    )
+                    ->perDay()
+                    ->count(),
+                'month' => $data = Trend::model(Booking::class)
+                    ->between(
+                        start: now()->subMonth(),
+                        end: now(),
+                    )
+                    ->perDay()
+                    ->count(),
+                '3months' => $data = Trend::model(Booking::class)
+                    ->between(
+                        start: now()->subMonths(3),
+                        end: now(),
+                    )
+                    ->perMonth()
+                    ->count(),
+            };
+        }
 
         return [
             'datasets' => [
                 [
-                    'label' => 'Bookings',
+                    'label' => 'Data',
                     'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
                 ],
             ],
@@ -73,6 +107,6 @@ class BlogPostsChart extends ChartWidget
 
     protected function getType(): string
     {
-        return 'bar';
+        return 'line';
     }
 }
